ab2835a704e46bfbdb06d8045dadd418ec98744a
alokshakya <alokshakyacode@gmail.com>
2020-04-26 18:49:27 +0530

    created components for required functionality via cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SidebarComponent,
+    HeaderComponent,
+    GameCardComponent
   ],
   imports: [
     BrowserModule,
